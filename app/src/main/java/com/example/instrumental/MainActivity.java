1184dd9b091726bd18e237c500271cbfa4322b4d
Ava Smith <ava.smith103@gmail.com>
2021-04-26 21:07:34 -0500

    Added simple event handling for first two pages

--- app/src/main/java/com/example/instrumental/MainActivity.java
+++ app/src/main/java/com/example/instrumental/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.instrumental;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -17,16 +21,65 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private GoogleMap mMap;
 
+    private Button button1;
+    private Button button2;
+    private EditText editText1;
+    private EditText editText2;
+
     private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.borrower_alternate);
+        setContentView(R.layout.login_layout);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        // Remap buttons for each view change
+        button1 = (Button) findViewById(R.id.login_register);
+        button2 = (Button) findViewById(R.id.login_sign_in);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                EditText editText1 = (EditText) findViewById(R.id.login_username);
+                EditText editText2 = (EditText) findViewById(R.id.login_password);
+
+                String username = editText1.getText().toString().replace(" ", "");
+                String password = editText2.getText().toString().replace(" ", "");
+
+                System.out.println(username);
+                System.out.println(password);
+                if(username.equals("admin") && password.equals("password")){
+                    Toast.makeText(MainActivity.this, "Yes :) Logging you in", Toast.LENGTH_SHORT).show();
+                    setContentView(R.layout.whoareyou_layout);
+                    setupWhoareyou();
+                }else{
+                    Toast.makeText(MainActivity.this, "Credentials incorrect. Hint, please try admin and password", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        //SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        //mapFragment.getMapAsync(this);
         //imageView = (ImageView) findViewById(R.id.imageView);
         //imageView.setImageResource(R.drawable.instrumental);
+    }
+
+    public void setupWhoareyou(){
+        System.out.println("got here");
+        button1 = (Button) findViewById(R.id.who_borrow_button);
+        button2 = (Button) findViewById(R.id.who_lend_button);
+
+        button1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                setContentView(R.layout.borrower_main);
+                //setupBorrowerMain();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                setContentView(R.layout.lender_side);
+                //setupLenderSide();
+            }
+        });
     }
 
     @Override
